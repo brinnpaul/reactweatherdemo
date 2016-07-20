1485b240e80d0c4b7b4507776ea9a3d650db0a90
Brinn Riordan <brinn.riordan@gmail.com>
2016-07-20 14:54:13 -0400

    middleware http redirect

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://'+ req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, ()=> {
-  console.log("express server running on port 3000");
-})
+app.listen(PORT, ()=> {
+  console.log("express server running on port"+PORT);
+});
